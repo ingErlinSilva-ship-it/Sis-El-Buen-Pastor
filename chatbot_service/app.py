2a7e51c30215b6b5760eb8d3df5dc5b61d22167b
Ing Erlin Silva <ing.carmonaer@gmail.com>
2026-02-14 12:45:55 -0600

    se cambio el host para el chatbot

--- chatbot_service/app.py
+++ chatbot_service/app.py
@@ -26,10 +26,11 @@ MODEL_NAME = "gemini-2.5-flash"
 # ==============================
 
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'clinicadb'
+    'host': os.getenv('DB_HOST'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'database': os.getenv('DB_NAME'),
+    'port': 3306
 }
 
 INFO_CLINICA = {
