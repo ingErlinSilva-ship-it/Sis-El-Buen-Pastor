e575c60996a995dea52a1e3efd332691c5c6cef1
Ing Erlin Silva <ing.carmonaer@gmail.com>
2026-02-23 21:43:30 -0600

    Configuracion del correov2

--- routes/web.php
+++ routes/web.php
@@ -74,6 +74,5 @@ Route::middleware(['auth'])->group(function () {
 
 // Ruta para el Chatbot.
 Route::post('/chatbot/consulta', [ChatbotController::class, 'consulta'])->name('chatbot.consulta');
-Route::post('/citas/chatbot', [CitaController::class, 'store']);
 
 require __DIR__.'/auth.php';
